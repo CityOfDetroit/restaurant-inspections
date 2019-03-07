bdcad97d631921b1327f6028f9b1a544171b840e
jessica <jessica.mcinchak@gmail.com>
2019-03-06 19:41:10 -0500

    donut chart just for fun

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Header, Divider, Statistic } from "semantic-ui-react"
+import { Header, Divider, Statistic, Grid, Container } from "semantic-ui-react"
+import { RadialChart } from "react-vis"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const AboutPage = ({ data }) => {
-  const stats = [
-    { key: 'establishments', label: 'Establishments', value: `${data.postgres.allEstablishments.totalCount.toLocaleString()}`, color: 'violet'},
-    { key: 'inspections', label: 'Inspections', value: `${data.postgres.allInspections.totalCount.toLocaleString()}`, color: 'purple'},
-    { key: 'violations', label: 'Violations', value: `${data.postgres.allViolations.totalCount.toLocaleString()}`, color: 'pink'},
+  const complianceData = [
+    { label: 'Compliant', angle: data.postgres.compliantInspections.totalCount, color: '#21BA45' },
+    { angle: data.postgres.allInspections.totalCount - data.postgres.compliantInspections.totalCount, color: '#DB2828' }
   ]
 
   return (
@@ -18,10 +18,37 @@ const AboutPage = ({ data }) => {
       <Header as='h1'>About</Header>
       <Divider />
       <Header as='h2'>What's in this data?</Header>
-      <section style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-        <Statistic.Group items={stats} />
-      </section>
-      <Divider />
+      <Grid columns={3} divided>
+        <Grid.Column>
+          <Statistic color='violet'>
+            <Statistic.Value>{data.postgres.allEstablishments.totalCount.toLocaleString()}</Statistic.Value>
+            <Statistic.Label>Establishments</Statistic.Label>
+          </Statistic>
+        </Grid.Column>
+        <Grid.Column>
+          <Statistic color='purple'>
+            <Statistic.Value>{data.postgres.allInspections.totalCount.toLocaleString()}</Statistic.Value>
+            <Statistic.Label>Inspections</Statistic.Label>
+          </Statistic>
+          <Container>
+            <RadialChart 
+              height={200} 
+              width={200} 
+              data={complianceData}
+              colorType='literal'
+              innerRadius={70}
+              radius={90}
+              padAngle={0.04}
+              showLabels />
+          </Container>
+        </Grid.Column>
+        <Grid.Column>
+          <Statistic color='pink'>
+            <Statistic.Value>{data.postgres.allViolations.totalCount.toLocaleString()}</Statistic.Value>
+            <Statistic.Label>Violations</Statistic.Label>
+          </Statistic>
+        </Grid.Column>
+      </Grid>
       <Header as='h2'>Frequently Asked Questions</Header>
     </Layout>
   )
