06980927475d13b850a29534ab20e319408b5cde
jessica <jessica.mcinchak@gmail.com>
2019-04-24 14:59:16 -0400

    update config icon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,10 +26,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#9fd5b3`,
+        theme_color: `#9fd5b3`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cod-logo.jpg`, // This path is relative to the root of the site.
       },
     },
     {
