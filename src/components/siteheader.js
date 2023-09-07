acf0103216830f9c51efe74332bea3e389ccf492
Jacob Yesh-Brochstein <Jacob.Yesh-Brochstein@detroitmi.gov>
2023-09-07 11:21:03 -0400

    comment out import

--- src/components/siteheader.js
+++ src/components/siteheader.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { Image, Header } from "semantic-ui-react"
-import PropTypes from "prop-types"
+//import PropTypes from "prop-types"
 import _ from "lodash"
 
 import  logo  from "../images/cod-logo.jpg"
