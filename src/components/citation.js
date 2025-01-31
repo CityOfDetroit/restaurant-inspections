13332c642303f2ffd71c478468bb8aa98a4991da
Jimmy McBroom <james.mcbroom@gmail.com>
2025-01-30 23:47:48 -0500

    show true build date instead of calculating

--- src/components/citation.js
+++ src/components/citation.js
@@ -5,7 +5,7 @@ import metadata from "../data/metadata";
 
 const Citation = ({ date }) => (
   <Segment basic size='small' textAlign='right'>
-    Built using <a href={metadata.links.openData.url} target='_blank' rel='noopener noreferrer'>{metadata.links.openData.display}</a> | Site last updated {dayjs(date).add(30, 'day').format('MM-DD-YYYY')} with inspection data thru {dayjs(date).format('MM-DD-YYYY')}.
+    Built using <a href={metadata.links.openData.url} target='_blank' rel='noopener noreferrer'>{metadata.links.openData.display}</a> | Site last updated {dayjs().format('MM-DD-YYYY')} with inspection data thru {dayjs(date).format('MM-DD-YYYY')}.
   </Segment>
 );
 
